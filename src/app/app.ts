4ef7aeac0d52b397ed43166788b237a1d6666faa
Hamza Mohamed Adel <hamzaawy.code@gmail.com>
2025-12-01 13:48:45 +0200

    first commit and Creating a suitable work environment

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FlowbiteService } from './core/services/flowbite.service';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = signal('exchangeRate');
+  protected readonly _flowBiteService = inject(FlowbiteService);
+
+  ngOnInit(): void {
+    this._flowBiteService.loadFlowbite((flowbite) => {
+      initFlowbite();
+    });
+  }
 }
